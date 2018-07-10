01f2d36f03c4ec63946b02d7fcf910387ebc04a2
Hodga <hojajepbar@gmail.com>
2018-07-10 19:24:10 +0300

    Complited header javascript

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -1,14 +1,20 @@
 import $ from 'jquery';
 
 class MobileMenu {
+
 	constructor(){
+		this.siteHeader = $('.site-header');
 		this.menuIcon = $('.site-header__menu-icon');
+		this.menuContent = $('.site-header__menu-content');
+		this.event();
 	}
 	event() {
-		this.menuIcon.click();
+		this.menuIcon.click(this.toggleTheMenu.bind(this));
 	}
 	toggleTheMenu(){
-		console.log('Hooray icon was clicked');
+		this.menuContent.toggleClass('site-header__menu-content--is-visible');
+		this.siteHeader.toggleClass('site-header--is-expanded');
+		this.menuIcon.toggleClass('site-header__menu-icon--close-x');
 	}
 }
 
